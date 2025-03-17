c67dbec4ff9156e956f2d294f044daf4b33a98d7
abhishek <abhimangalur1@gmail.com>
2025-03-17 08:13:07 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 dotenv.config();
 connectDB();
